43b50444cebb64cb22a33c6383a92c53ea477ae2
GregoryBilliel <gjb1039@usnh.edu>
2026-04-24 16:27:03 -0400

    yes

--- code/02_cutadapt.sh
+++ code/02_cutadapt.sh
@@ -27,13 +27,14 @@ cutadapt_config="--p-front-f $fw --p-front-r $rv"
 qiime cutadapt trim-paired \
     --i-demultiplexed-sequences data/results/${projname}_demux.qza \
     --p-error-rate 0.12 \
-    --o-trimmed-sequences results/${projname}_demux_cutadapt.qza \
+    --o-trimmed-sequences data/results/${projname}_demux_cutadapt.qza \
     --p-cores 4 \
-    "${cutadapt_config}" \
+    --p-front-f ^GTGYCAGCMGCCGCGGTAA \
+    --p-front-r ^CCGYCAATTYMTTTRAGTTT \
     --p-discard-untrimmed \
     --p-match-adapter-wildcards \
     --verbose 
 
 qiime demux summarize \
-    --i-data results/${projname}_demux_cutadapt.qza \
-    --o-visualization results/${projname}_demux_cutadapt.qzv
+    --i-data data/results/${projname}_demux_cutadapt.qza \
+    --o-visualization data/results/${projname}_demux_cutadapt.qzv
