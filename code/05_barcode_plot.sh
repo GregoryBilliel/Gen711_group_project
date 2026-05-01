c81dd35193a1552251dd72130240d73484eff273
GregoryBilliel <gjb1039@usnh.edu>
2026-05-01 13:34:56 -0400

    Saving for after exam

--- code/05_barcode_plot.sh
+++ code/05_barcode_plot.sh
@@ -2,6 +2,9 @@
 
 ## IF THIS DOES NOT WORK, TRY AND PUT data/ BEFORE THE metadata/ ON LINES 13, 30 and 49!!!
 
+primer="16s_V4-V5"
+projname="Cyanobacteria_${primer}"
+
 ## replace with the qiime2 environment youve been using 
 conda activate qiime2-amplicon-2026.1
 
@@ -10,8 +13,14 @@ conda activate qiime2-amplicon-2026.1
 qiime taxa barplot \
   --i-table data/results/${projname}_table.qza \
   --i-taxonomy data/results/${projname}_hybrid_taxonomy.qza \
-  --m-metadata-file data/metadata/metadata-update.tsv \
+  --m-metadata-file data/metadata/pico-mdat.tsv \
   --o-visualization data/results/${projname}_taxa_barplot.qzv
+
+## ERROR WHEN I TRIED TO RUN IT:
+## Plugin error from taxa:
+
+ ## Sample IDs found in the table are missing in the metadata: {'WLW-VP', '18-1-100', '32-1-100', '26-1-100', 'PAC', 'WLW-TK', 'AIR-EW', 'AIR-VP', 'AIR-TK', 'XB', '28-1-100', '40-100', 'NTC', 'WLW-EW', '29-1-100', '21-1-100', '23-1-100', '3-1-100', '24-1-100', '2-1-100', '12-1-100'}.
+##Debug info has been saved to /tmp/qiime2-q2cli-err-cjdwf66f.log
 
 ## To view the interactive barplot, you can use the qiime2 view command or upload the .qzv file to https://view.qiime2.org/ to interactively explore the plot. You can also export the plot as a .png file. Screenshots of the barplots work as well
 ## To download the .qzv file, right click on the file in vscode to download it to your local computer, then you can upload it to the qiime2 view website.
@@ -23,12 +32,20 @@ qiime feature-table filter-features \
   --i-table data/results/${projname}_table.qza \
   --m-metadata-file data/results/${projname}_hybrid_taxonomy.qza \
   --o-filtered-table data/results/${projname}_taxonomy-matched-table.qza
-  
+
+## In the --i-table input I think the file is wrong for this one, the output above gives taxonomy-matched-table.qza while the input for this second command needs a filtered-table.qza. Still think the issue is a metadata file issue vs a command issue
+
 qiime taxa barplot \
-  --i-table data/results/${projname}_filtered-table.qza \
+  --i-table data/results/${projname}_taxonomy-matched-table.qza \
   --i-taxonomy data/results/${projname}_hybrid_taxonomy.qza \
-  --m-metadata-file data/metadata/metadata-update.tsv \
+  --m-metadata-file data/metadata/pico-mdat.tsv \
   --o-visualization plots/${projname}_taxa_barplot.qzv
+
+  ## still getting error: Plugin error from taxa:
+
+ ## Sample IDs found in the table are missing in the metadata: {'XB', 'AIR-VP', 'PAC', '29-1-100', '18-1-100', '28-1-100', '32-1-100', '3-1-100', 'AIR-EW', 'WLW-TK', '24-1-100', '40-100', '12-1-100', 'WLW-EW', 'AIR-TK', '26-1-100', '21-1-100', '2-1-100', 'NTC', '23-1-100', 'WLW-VP'}.
+
+## Debug info has been saved to /tmp/qiime2-q2cli-err-ckslvew0.log
 
 ## Make a phylogenetic tree and run core metrics to get the alpha and beta diversity metrics for each sample. This will be used in the next script to create a PCoA plot of the beta diversity metrics.
 qiime phylogeny align-to-tree-mafft-fasttree \
@@ -38,6 +55,8 @@ qiime phylogeny align-to-tree-mafft-fasttree \
    --o-tree data/results/${projname}_unrooted-tree.qza\
    --o-rooted-tree data/results/${projname}_rooted-tree.qza\
    --p-n-threads 24
+
+## --m-metadata-file part is screwy, will more then likely need to change thats
 
 ### Core Metrics (this will generate the alpha and beta diversity metrics for each sample, which will be used in the next script to create a PCoA plot of the beta diversity metrics)
 ### Choose one diversity ordination to vizualize in the readme of your github. Justify why you chose that one. You can also make multiple ordination plots if you want to compare the different beta diversity metrics.
